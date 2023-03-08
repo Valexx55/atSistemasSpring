005d4e1561a00952b7f242fa55ffa4352786525c
valexx55 <valexx55@gmail.com>
2023-03-08 19:34:25 +0100

    subida reactiva

--- msalumnosprofe/src/main/java/edu/val/cle/atsistemas/services/AlumnoServiceImpl.java
+++ msalumnosprofe/src/main/java/edu/val/cle/atsistemas/services/AlumnoServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.val.cle.atsistemas.model.entity.Alumno;
 import edu.val.cle.atsistemas.repository.AlumnoRepository;
@@ -11,31 +12,49 @@ import edu.val.cle.atsistemas.repository.AlumnoRepository;
 @Service
 public class AlumnoServiceImpl implements AlumnoService{
 	
+	
+	/**
+	 * comprarBillete
+	 * {
+	 *  
+	 *  MARCAS EL ASIENTO COMO OCUPADO
+	 *  ACTUALIZA CONTABILIDAD
+	 *  PAGAS
+	 *  
+	 *  
+	 * }
+	 */
+	
 	@Autowired
 	private AlumnoRepository alumnoRepository;
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<Alumno> findAll() {
 		return this.alumnoRepository.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Optional<Alumno> findById(Long id) {
 		return this.alumnoRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Alumno save(Alumno alumno) {
 		return this.alumnoRepository.save(alumno);
 	}
 
 	@Override
+	@Transactional
 	public void deleteById(Long id) {
 		this.alumnoRepository.deleteById(id);	
 		
 	}
 
 	@Override
+	@Transactional
 	public Alumno update(Alumno alumno, Long id) {
 		Optional<Alumno> oa = null;
 		Alumno alumno_leido = null;
---
+++ msalumnosproferx/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ID_NAME(id SERIAL PRIMARY KEY, nombre VARCHAR(32), apellido VARCHAR(32), email VARCHAR(32), edad INT);
+INSERT INTO ALUMNO(nombre, apellido, email, edad) VALUES ('vale', 'more1', 'val1@gmail.es', 34);
+INSERT INTO ALUMNO(nombre, apellido, email, edad) VALUES ('vale1', 'more2', 'val2@gmail.es', 34);
+INSERT INTO ALUMNO(nombre, apellido, email, edad) VALUES ('vale2', 'more3', 'val3@gmail.es', 34);
+INSERT INTO ALUMNO(nombre, apellido, email, edad) VALUES ('vale3', 'more4', 'val4@gmail.es', 34);
+INSERT INTO ALUMNO(nombre, apellido, email, edad) VALUES ('vale4', 'more', 'val5@gmail.es', 34);
+
